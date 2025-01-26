4a6206e3c0b3be0375fd414775c5511a2bb22d31
John Johnstone <jjohnstone@riseup.net>
2025-01-26 05:58:59 +0000

    fix formatting

--- src/browser/qutebrowser.rs
+++ src/browser/qutebrowser.rs
@@ -3,7 +3,7 @@ use rusqlite::Connection;
 use uuid::Uuid;
 
 pub struct History {
-    pub entries: Vec<HistoryEntry>
+    pub entries: Vec<HistoryEntry>,
 }
 
 pub struct HistoryEntry {
@@ -28,25 +28,29 @@ pub fn locate_database() -> Option<Database> {
     if let Ok(qute_data_dir) = std::env::var("QUTE_DATA_DIR") {
         let basepath = Path::new(&qute_data_dir);
 
-        let path = basepath
-            .join("history.sqlite");
+        let path = basepath.join("history.sqlite");
         info!("checking path: {}", &path.as_os_str().to_str().unwrap());
         if path.exists() {
-            Some(Database { location: path, tmp_location: None })
+            Some(Database {
+                location: path,
+                tmp_location: None,
+            })
         } else {
             None
         }
     } else {
         let home = std::env::var("HOME").unwrap();
-	let basepath = Path::new(&home).join(".local/share/qutebrowser");
-	let path = basepath
-	    .join("history.sqlite");
-	info!("checking path: {}", &path.as_os_str().to_str().unwrap());
-	if path.exists() {
-	    Some(Database { location: path, tmp_location: None })
-	} else {
-	    None
-	}
+        let basepath = Path::new(&home).join(".local/share/qutebrowser");
+        let path = basepath.join("history.sqlite");
+        info!("checking path: {}", &path.as_os_str().to_str().unwrap());
+        if path.exists() {
+            Some(Database {
+                location: path,
+                tmp_location: None,
+            })
+        } else {
+            None
+        }
     }
 }
 
@@ -64,22 +68,26 @@ pub fn get_history(database: Database) -> History {
     info!("{}", &db_path.as_os_str().to_str().unwrap());
     let conn = Connection::open(db_path).unwrap();
 
-    let mut history_query = conn.prepare("SELECT title, url, atime, redirect FROM History").unwrap();
+    let mut history_query = conn
+        .prepare("SELECT title, url, atime, redirect FROM History")
+        .unwrap();
 
-    let history_rows = history_query.query_map([], |row| {
-        Ok(HistoryEntry {
-            title: row.get(0).unwrap(),
-            url: row.get(1).unwrap(),
-            access_time: row.get(2).unwrap(),
-            redirect: row.get(3).unwrap(),
+    let history_rows = history_query
+        .query_map([], |row| {
+            Ok(HistoryEntry {
+                title: row.get(0).unwrap(),
+                url: row.get(1).unwrap(),
+                access_time: row.get(2).unwrap(),
+                redirect: row.get(3).unwrap(),
+            })
         })
-    }).unwrap();
+        .unwrap();
 
     let mut history = Vec::new();
 
-    history_rows.into_iter().for_each(|a| {
-        history.push(a.unwrap())
-    });
+    history_rows
+        .into_iter()
+        .for_each(|a| history.push(a.unwrap()));
 
     History { entries: history }
 }
